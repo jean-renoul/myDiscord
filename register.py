94469f46bb945af409120255abefd6553b044ca2
maxence-vaisse <maxence.vaisse@laplateforme.io>
2024-02-07 12:43:28 +0100

    Ajout de la classe Register

--- register.py
+++ register.py
@@ -1,12 +1,23 @@
+import tkinter
 from tkinter import * # Import tkinter
 
-windows = Tk() # Creation de la fenetre tkinter
-windows.title("Register") # Titre de la fenetre
-windows.geometry("900x540") # Taille de la fenetre
-windows.resizable(False, False) # Taille de la fenetre non modifiable
 
-# Ajout de la couleur de fond
-frame = Frame(windows, width=900, height=540, bg="#2c2f33", bd=5)
-frame.place(x=0, y=0)
+class Register:
+    def __init__(self, windows):
+        self.windows = windows
+        self.windows.title("Register")
+        self.windows.geometry("900x540")
+        self.windows.resizable(False, False)
 
-windows.mainloop() # Boucle principale
+        # Ajout de la couleur de fond
+        frame = Frame(windows, width=900, height=540, bg="#2c2f33", bd=5)
+        frame.place(x=0, y=0)
+
+        # Titre de la page
+        heading = Label(frame, text="Register", fg="white", bg="#2c2f33", font=("Segoe UI", 20))
+        heading.place(x=400, y=10)
+
+if __name__ == "__main__":
+    windows = Tk()  # Création de l'objet Tk
+    app = Register(windows)  # Création de l'instance de la classe Register
+    windows.mainloop()  # Boucle principale
