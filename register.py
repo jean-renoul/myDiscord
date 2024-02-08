f2eb432a1530120c8b48004d52bd26b115185ed3
maxence-vaisse <maxence.vaisse@laplateforme.io>
2024-02-08 08:56:15 +0100

    Enlèvement des bordure pour chaque entré de texte

--- register.py
+++ register.py
@@ -20,25 +20,25 @@ class Register:
         # Prénom
         firstname = Label(frame, text="Prénom :", fg="#7289da", bg="#2c2f33", font=("Segoe UI", 17))
         firstname.place(x=300, y=100)
-        firstname_entry = Entry(frame, width=30, borderwidth=2,)
+        firstname_entry = Entry(frame, width=30,) # border-radius=2 pour arrondir les bords (Windows uniquement)
         firstname_entry.place(x=302, y=140)
 
         # Nom
         lastname = Label(frame, text="Nom :", fg="#7289da", bg="#2c2f33", font=("Segoe UI", 17))
         lastname.place(x=300, y=170)
-        lastname_entry = Entry(frame, width=30, borderwidth=2)
+        lastname_entry = Entry(frame, width=30,)
         lastname_entry.place(x=302, y=210)
 
         # Email
         email = Label(frame, text="Email :", fg="#7289da", bg="#2c2f33", font=("Segoe UI", 17))
         email.place(x=300, y=240)
-        email_entry = Entry(frame, width=30, borderwidth=2)
+        email_entry = Entry(frame, width=30,)
         email_entry.place(x=302, y=280)
 
         # Mot de passe
         password = Label(frame, text="Mot de passe :", fg="#7289da", bg="#2c2f33", font=("Segoe UI", 17))
         password.place(x=300, y=310)
-        password_entry = Entry(frame, width=30, borderwidth=2, show="*") # show="*" pour cacher le mot de passe
+        password_entry = Entry(frame, width=30, show="*") # show="*" pour cacher le mot de passe
         password_entry.place(x=302, y=350)
 
 
