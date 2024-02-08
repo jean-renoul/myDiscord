38025d7eb0c19c2a1460f75b7b2f8d9ca1cc974a
maxence-vaisse <maxence.vaisse@laplateforme.io>
2024-02-08 14:00:07 +0100

    Ajout de la couleur de fond

--- login.py
+++ login.py
@@ -8,6 +8,10 @@ class Login:
         self.windows.geometry("600x300")
         self.windows.resizable(False, False)
 
+        # Ajout de la couleur de fond
+        frame = Frame(self.windows, width=600, height=300, bg="#2c2f33", bd=5)
+        frame.place(x=0, y=0)
+
 if __name__ == "__main__":
         app = Login()  # Création de l'instance de la classe Login
         app.windows.mainloop()  # Lancement de la boucle principale
