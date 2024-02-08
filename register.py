37a0562c3fd14010f77f03b4b4288c82e004e137
maxence-vaisse <maxence.vaisse@laplateforme.io>
2024-02-08 12:13:01 +0100

    Ajout de la def submit qui permet de pas valider si un champs est pas remplie

--- register.py
+++ register.py
@@ -1,16 +1,16 @@
-import tkinter
+import tkinter as tk
 from tkinter import * # Import tkinter
-
+from tkinter import messagebox # Import messagebox
 
 class Register:
-    def __init__(self, windows):
-        self.windows = windows
+    def __init__(self):
+        self.windows = tk.Tk()  # Création de l'objet Tk
         self.windows.title("Register")
         self.windows.geometry("900x540")
         self.windows.resizable(False, False)
 
         # Ajout de la couleur de fond
-        frame = Frame(windows, width=900, height=540, bg="#2c2f33", bd=5)
+        frame = Frame(self.windows, width=900, height=540, bg="#2c2f33", bd=5)
         frame.place(x=0, y=0)
 
         # Titre de la page
@@ -18,35 +18,49 @@ class Register:
         heading.place(x=300, y=10)
 
         # Prénom
-        firstname = Label(frame, text="Prénom :", fg="#7289da", bg="#2c2f33", font=("Segoe UI", 17))
-        firstname.place(x=300, y=100)
-        firstname_entry = Entry(frame, width=30,) # border-radius=2 pour arrondir les bords (Windows uniquement)
-        firstname_entry.place(x=302, y=140)
+        self.firstname = Label(frame, text="Prénom :", fg="#7289da", bg="#2c2f33", font=("Segoe UI", 17))
+        self.firstname.place(x=300, y=100)
+        self.firstname_entry = Entry(frame, width=30,) # border-radius=2 pour arrondir les bords (Windows uniquement)
+        self.firstname_entry.place(x=302, y=140)
 
         # Nom
-        lastname = Label(frame, text="Nom :", fg="#7289da", bg="#2c2f33", font=("Segoe UI", 17))
-        lastname.place(x=300, y=170)
-        lastname_entry = Entry(frame, width=30,)
-        lastname_entry.place(x=302, y=210)
+        self.lastname = Label(frame, text="Nom :", fg="#7289da", bg="#2c2f33", font=("Segoe UI", 17))
+        self.lastname.place(x=300, y=170)
+        self.lastname_entry = Entry(frame, width=30,)
+        self.lastname_entry.place(x=302, y=210)
 
         # Email
-        email = Label(frame, text="Email :", fg="#7289da", bg="#2c2f33", font=("Segoe UI", 17))
-        email.place(x=300, y=240)
-        email_entry = Entry(frame, width=30,)
-        email_entry.place(x=302, y=280)
+        self.email = Label(frame, text="Email :", fg="#7289da", bg="#2c2f33", font=("Segoe UI", 17))
+        self.email.place(x=300, y=240)
+        self.email_entry = Entry(frame, width=30,)
+        self.email_entry.place(x=302, y=280)
 
         # Mot de passe
-        password = Label(frame, text="Mot de passe :", fg="#7289da", bg="#2c2f33", font=("Segoe UI", 17))
-        password.place(x=300, y=310)
-        password_entry = Entry(frame, width=30, show="*") # show="*" pour cacher le mot de passe
-        password_entry.place(x=302, y=350)
+        self.password = Label(frame, text="Mot de passe :", fg="#7289da", bg="#2c2f33", font=("Segoe UI", 17))
+        self.password.place(x=300, y=310)
+        self.password_entry = Entry(frame, width=30, show="*") # show="*" pour cacher le mot de passe
+        self.password_entry.place(x=302, y=350)
 
         # Bouton d'inscription
-        register_button = Button(frame, text="Valider l'inscription", width=20, borderwidth=0, bg="#7289da", fg="white", font=("Segoe UI", 15))
+        register_button = Button(frame, text="Valider l'inscription", width=20, borderwidth=0, bg="#7289da", fg="white", font=("Segoe UI", 15), command=self.submit)
         register_button.place(x=300, y=400)
 
+    def submit(self):
+        if self.firstname_entry.get() == "":
+            messagebox.showerror("Erreur", "Veuillez entrer votre prénom")
+            return
+        elif self.lastname_entry.get() == "":
+            messagebox.showerror("Erreur", "Veuillez entrer votre nom")
+            return
+        elif self.email_entry.get() == "":
+            messagebox.showerror("Erreur", "Veuillez entrer votre email")
+            return
+        elif self.password_entry.get() == "":
+            messagebox.showerror("Erreur", "Veuillez entrer votre mot de passe")
+            return
+        else:
+            pass
 
 if __name__ == "__main__":
-    windows = Tk()  # Création de l'objet Tk
-    app = Register(windows)  # Création de l'instance de la classe Register
-    windows.mainloop()  # Boucle principale
+    app = Register()  # Création de l'instance de la classe Register
+    app.windows.mainloop()  # Boucle principale
