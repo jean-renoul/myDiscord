231d858eb9206094e2825111be799f91e7ad1544
maxence-vaisse <maxence.vaisse@laplateforme.io>
2024-02-09 08:41:43 +0100

    Ajout du Titre de la page

--- login.py
+++ login.py
@@ -13,6 +13,10 @@ class Login:
         frame = Frame(self.windows, width=600, height=300, bg="#2c2f33", bd=5)
         frame.place(x=0, y=0)
 
+        # Titre de la page
+        heading = Label(frame, text="Se connecter", fg="white", bg="#2c2f33", font=("Segoe UI", 30))
+        heading.place(x=180, y=10)
+
         # Mail
         Logo_Mail = Image.open("images/logo_mail.png")
         Logo_Mail = Logo_Mail.resize((30, 30), Image.LANCZOS)
