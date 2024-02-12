87ae8465b339fa7408cc16ad090880553412303d
jean-renoul <jean.renoul@laplateforme.io>
2024-02-12 09:23:57 +0100

    ajout des classes

---
+++ Channel.py
@@ -0,0 +1,16 @@
+class channel:
+    def __init__(self, name):
+        self.name = name
+        self.users = set()
+
+    def addUser(self, clientSocket):
+        self.users.add(clientSocket)
+
+    def removeUser(self, user):
+        self.users.remove(user)
+
+    def sendMessage(self, message):
+        for clientSocket in self.users:
+            clientSocket.send(message.encode())
+
+    
