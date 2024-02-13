66fd520c9a6f290f65b035a5848db77727291dd6
maxence-vaisse <maxence.vaisse@laplateforme.io>
2024-02-13 09:38:44 +0100

    Centrage de tous les élèments

--- Class/register.py
+++ Class/register.py
@@ -16,30 +16,30 @@ class Register:
         frame.place(x=0, y=0)
 
         heading = Label(frame, text="S'inscrire sur Discord", fg="white", bg="#2c2f33", font=("Segoe UI", 30))
-        heading.place(x=300, y=10)
+        heading.place(x=250, y=10)
 
         self.firstname = Label(frame, text="Prénom :", fg="#7289da", bg="#2c2f33", font=("Segoe UI", 17))
-        self.firstname.place(x=300, y=100)
+        self.firstname.place(x=340, y=100)
         self.firstname_entry = Entry(frame, width=30)
-        self.firstname_entry.place(x=302, y=140)
+        self.firstname_entry.place(x=342, y=140)
 
         self.lastname = Label(frame, text="Nom :", fg="#7289da", bg="#2c2f33", font=("Segoe UI", 17))
-        self.lastname.place(x=300, y=170)
+        self.lastname.place(x=340, y=170)
         self.lastname_entry = Entry(frame, width=30)
-        self.lastname_entry.place(x=302, y=210)
+        self.lastname_entry.place(x=342, y=210)
 
         self.email = Label(frame, text="Email :", fg="#7289da", bg="#2c2f33", font=("Segoe UI", 17))
-        self.email.place(x=300, y=240)
+        self.email.place(x=340, y=240)
         self.email_entry = Entry(frame, width=30)
-        self.email_entry.place(x=302, y=280)
+        self.email_entry.place(x=342, y=280)
 
         self.password = Label(frame, text="Mot de passe :", fg="#7289da", bg="#2c2f33", font=("Segoe UI", 17))
-        self.password.place(x=300, y=310)
+        self.password.place(x=340, y=310)
         self.password_entry = Entry(frame, width=30, show="*")
-        self.password_entry.place(x=302, y=350)
+        self.password_entry.place(x=342, y=350)
 
         register_button = Button(frame, text="Valider l'inscription", width=20, borderwidth=0, bg="#7289da", fg="white", font=("Segoe UI", 15), command=self.submit)
-        register_button.place(x=300, y=400)
+        register_button.place(x=325, y=400)
 
     def submit(self):
         firstname = self.firstname_entry.get()
